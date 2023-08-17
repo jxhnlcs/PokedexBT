eff8fea0c8b01d6a25024807a3893bd58bd2d272
John Lucas <joohnzay@gmail.com>
2023-08-17 15:01:39 -0300

    estilização incial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Inicio</h1>
+  <section class="container">
+    <h1>Pokédex</h1>
+    <ol>
+      <li>
+        Bulbasaur
+      </li>
+      <li>
+        Ivysaur
+      </li>
+      <li>
+        Venusaur
+      </li>
+    </ol>
+  </section>
 </template>
 
 <script lang="ts">
@@ -11,5 +24,13 @@ export default defineComponent({
 </script>
 
 <style scoped>
+
+.container{
+  padding: 1rem;
+}
+
+.container h1{
+  margin: 0;
+}
 
 </style>
