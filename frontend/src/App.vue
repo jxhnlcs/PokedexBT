456e177f75d0d8979e16b37416a7b4bcf04800da
John Lucas <joohnzay@gmail.com>
2023-08-18 12:54:20 -0300

    adicionando imagem

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,21 +4,27 @@
     <ol class="pokemons">
       <li>
         Bulbasaur
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
       </li>
       <li>
         Ivysaur
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="Ivysaur">
       </li>
       <li>
         Venusaur
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg" alt="Venusaur">
       </li>
       <li>
         Charmander
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg" alt="Charmander">
       </li>
       <li>
         Charmeleon
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" alt="Charmeleon">
       </li>
       <li>
         Charizard
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg" alt="Charizard">
       </li>
     </ol>
   </section>
@@ -42,18 +48,24 @@ export default defineComponent({
   margin: 0;
 }
 .pokemons {
+  margin-top: 10px;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-auto-rows: 160px;
   grid-gap: .5rem;
   list-style: none;
   padding: 0;
-  margin: 0;
 }
 .pokemons li {
+  display: flex;
+  flex-direction: column;
   padding: 2rem 1rem;
   color: white;
   border-radius: 1rem;
   background-color: #14A06F;
+}
+
+.pokemons li img{
+  max-width: 100%;
+  height: 100px;
 }
 </style>
